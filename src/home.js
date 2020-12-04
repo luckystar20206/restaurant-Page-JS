dbb3b0baf4e31d3c5e7a7d5b9b987df5e3732a08
luckystar20206 <luckystar20206@gmail.com>
2020-12-04 08:17:46 -0500

    Add 'active' class to current tab

--- src/home.js
+++ src/home.js
@@ -1,9 +1,55 @@
+const homePage = document.querySelector('.content');
+
 const home = () => {
-    const homePage = document.querySelector('.content');
     while(homePage.firstChild) {
-        homePage.removeChild(firstChild);
+        homePage.removeChild(homePage.firstChild);
     }
-    homePage.classList.add('home-background');
+    const prevAct = document.querySelectorAll('.active');
+    prevAct.forEach(tag => {
+        tag.classList.remove('active');
+    });
+    const background = document.createElement('div');
+    background.classList.add('home-background');
+    homePage.appendChild(background);
+    const heroTitle = document.createElement('h1');
+    heroTitle.textContent = 'Majestic';
+    heroTitle.classList.add('hero-title');
+    homePage.appendChild(heroTitle);
+    const homeLink = document.getElementById('home');
+    homeLink.classList.add('active');
 }
 
-export { home }
+const menu = () => {
+    while(homePage.firstChild) {
+        homePage.removeChild(homePage.firstChild);
+    }
+
+    const prevAct = document.querySelectorAll('.active');
+    prevAct.forEach(tag => {
+        tag.classList.remove('active');
+    });
+    const menuLink = document.getElementById('menu');
+    menuLink.classList.add('active');
+
+    const background = document.createElement('div');
+    background.classList.add('menu-background');
+    homePage.appendChild(background);
+
+}
+
+const contact = () => {
+    while(homePage.firstChild) {
+        homePage.removeChild(homePage.firstChild);
+    }
+
+    const prevAct = document.querySelectorAll('.active');
+    prevAct.forEach(tag => {
+        tag.classList.remove('active');
+    });
+    const contactLink = document.getElementById('contact');
+    contactLink.classList.add('active');
+
+}
+
+
+export { home, menu, contact }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import "../assets/css/navbar.css";
 import "../assets/css/style.css";
 
 
-import { home } from "./home";
+import { home, menu, contact } from "./home";
 
 home();
+
+const homeLink = document.getElementById('home');
+const menuLink = document.getElementById('menu');
+const contactLink = document.getElementById('contact');
+
+menuLink.addEventListener('click', menu);
+
+homeLink.addEventListener('click', home);
+
+contactLink.addEventListener('click', contact);
